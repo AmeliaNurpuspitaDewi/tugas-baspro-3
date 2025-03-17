66dbcb8defaf5d7965b28b8996e86981643b67d4
AmeliaNurpuspitaDewi <amelie022018@gmail.com>
2025-03-17 13:26:30 +0700

    Update hitunggaji.py

--- hitunggaji.py
+++ hitunggaji.py
@@ -1,32 +1,48 @@
-nama = input("Masukkan Nama : ")
-nik = input("Masukkan NIK : ")
+nama = input("Masukkan Nama: ")
+nik = input("Masukkan NIK: ")
 
-status_list = ["Tetap", "Honor"]
+status_list = ["Pegawai Tetap", "Honor"]
 golongan_list = ["A", "B", "C"]
 
-gaji = {"Tetap": 1000000, "Honor": 750000}
-
-bonus = {
-    "Tetap": {"A": 200000, "B": 400000, "C": 550000},
-    "Honor": {"A": 150000, "B": 275000, "C": 480000},
+gaji_pokok = {
+    "Pegawai Tetap": 1000000,
+    "Honor": 750000
 }
 
-total_gaji_tetap = 0
-total_gaji_honor = 0
+bonus_golongan = {
+    "Pegawai Tetap": {"A": 200000, "B": 400000, "C": 550000},
+    "Honor": {"A": 150000, "B": 275000, "C": 480000}
+}
+
+
+total_gaji_per_status = {
+    "Pegawai Tetap": 0,
+    "Honor": 0
+}
+
+print("\n=== Daftar Gaji dan Bonus ===")
+print("Nama:", nama)
+print("NIK:", nik)
+print("===========================================")
 
 for status in status_list:
-    print("Status: " + status)
     for golongan in golongan_list:
-        total = gaji[status] + bonus[status][golongan]
-        print("  Golongan: " + golongan)
-        print("    Gaji Pokok: " + str(gaji[status]))
-        print("    Bonus: " + str(bonus[status][golongan]))
-        print("    Total Gaji: " + str(total) + "\n")
-        
-        if status == "Tetap":
-            total_gaji_tetap += total
-        else:
-            total_gaji_honor += total
+        gaji = gaji_pokok[status]
+        bonus = bonus_golongan[status][golongan]
+        gaji_total = gaji + bonus
 
-print("Total keseluruhan gaji untuk status Tetap: " + str(total_gaji_tetap))
-print("Total keseluruhan gaji untuk status Honor: " + str(total_gaji_honor))
+        total_gaji_per_status[status] += gaji_total
+
+        print("Status    :", status)
+        print("Golongan  :", golongan)
+        print("Gaji Pokok: Rp", gaji)
+        print("Bonus     : Rp", bonus)
+        print("Gaji Total: Rp", gaji_total)
+        print("-------------------------------------------")
+
+print("===========================================")
+
+print("Total Gaji + Bonus untuk setiap Status:")
+for status in total_gaji_per_status:
+    print(status, ": Rp", total_gaji_per_status[status])
+print("===========================================")
